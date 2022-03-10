b1420696483d417948272d21a7d721e46e7cfc8d
Deepanshu Tiwari <ddepu11@gmail.com>
2022-03-10 15:00:04 +0530

    chore: remove native projects

--- src/screens/ShowMessageScreen/ShowMessageScreenHeader.tsx
+++ src/screens/ShowMessageScreen/ShowMessageScreenHeader.tsx
@@ -1,40 +1,40 @@
-import { Box, Button, Image, Text } from 'native-base'
-import { FC } from 'react'
-import { AntDesign } from '@expo/vector-icons'
-import { Ionicons } from '@expo/vector-icons'
-import { FontAwesome } from '@expo/vector-icons'
-import { Platform } from 'react-native'
-import useShowMessageScreenHeaderLogic from './Logic/useShowMessageScreenHeaderLogic'
+import { Box, Button, Image, Text } from "native-base";
+import { FC } from "react";
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { Platform } from "react-native";
+import useShowMessageScreenHeaderLogic from "./Logic/useShowMessageScreenHeaderLogic";
 
 interface Props {
-  displayPicUrl: string
-  personName: string
-  otherPersonId: string
+  displayPicUrl: string;
+  personName: string;
+  otherPersonId: string;
 }
 
 const ShowMessageScreenHeader: FC<Props> = ({
   displayPicUrl,
   personName,
-  otherPersonId
+  otherPersonId,
 }) => {
   const { handleNavigateBack, hanleMakeVideoCall, hanleMakeVoice } =
-    useShowMessageScreenHeaderLogic(otherPersonId)
+    useShowMessageScreenHeaderLogic(otherPersonId);
 
   return (
     <Box
-      width={'100%'}
+      width={"100%"}
       bgColor="#FFFFFF"
-      flexDirection={'row'}
-      alignItems={'center'}
-      justifyContent={'space-between'}
-      color={'#000000'}
-      paddingY={'4'}
-      pt={Platform.OS === 'ios' ? 12 : 5}
+      flexDirection={"row"}
+      alignItems={"center"}
+      justifyContent={"space-between"}
+      color={"#000000"}
+      paddingY={"4"}
+      pt={Platform.OS === "ios" ? 12 : 5}
     >
-      <Box ml={'3'} flexDirection={'row'} alignItems={'center'}>
+      <Box ml={"3"} flexDirection={"row"} alignItems={"center"}>
         <Button
           padding={0}
-          bgColor={'transparent'}
+          bgColor={"transparent"}
           onPress={handleNavigateBack}
         >
           <AntDesign name="left" size={24} color="black" />
@@ -43,18 +43,18 @@ const ShowMessageScreenHeader: FC<Props> = ({
         <Image
           source={{ uri: displayPicUrl }}
           alt={personName}
-          width={'36'}
-          height={'36'}
+          width={"36"}
+          height={"36"}
           borderRadius={14}
-          ml={'4'}
+          ml={"4"}
         />
 
         <Text
-          fontSize={'16'}
-          fontWeight={'700'}
-          lineHeight={'23.5'}
-          color={'#000000'}
-          ml={'5'}
+          fontSize={"16"}
+          fontWeight={"700"}
+          lineHeight={"23.5"}
+          color={"#000000"}
+          ml={"5"}
         >
           {personName}
         </Text>
@@ -68,28 +68,28 @@ const ShowMessageScreenHeader: FC<Props> = ({
         </Text> */}
       </Box>
 
-      <Box flexDirection="row" textAlign={'center'} mr={'4'}>
+      <Box flexDirection="row" textAlign={"center"} mr={"4"}>
         <Button
           onPress={hanleMakeVoice}
-          bgColor={'#F4F4F4'}
-          padding={'2'}
+          bgColor={"#F4F4F4"}
+          padding={"2"}
           mr="4"
-          borderRadius={'2xl'}
+          borderRadius={"2xl"}
         >
           <Ionicons name="call-outline" size={24} color="#284180" />
         </Button>
 
         <Button
           onPress={hanleMakeVideoCall}
-          bgColor={'#F4F4F4'}
-          padding={'2'}
-          borderRadius={'2xl'}
+          bgColor={"#F4F4F4"}
+          padding={"2"}
+          borderRadius={"2xl"}
         >
           <FontAwesome name="video-camera" size={24} color="#284180" />
         </Button>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default ShowMessageScreenHeader
+export default ShowMessageScreenHeader;
