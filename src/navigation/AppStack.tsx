02b58d49030813210282f572c146af10c88e2bcf
Deepanshu Tiwari <ddepu11@gmail.com>
2022-03-15 12:43:02 +0530

    feat: update user doc on close/open app

--- src/navigation/AppStack.tsx
+++ src/navigation/AppStack.tsx
@@ -1,32 +1,71 @@
-import { FC, useEffect } from 'react'
+import { FC, useEffect, useRef, useState } from "react";
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import NavigationParams from '../types/navigationParams'
-import Routes from '../constants/routes'
-import BottomNavigationBar from './BottomNavigationBar'
-import ShowMessageScreen from '../screens/ShowMessageScreen/ShowMessageScreen'
-import DpAndUserName from '../screens/DpAndUserName'
-import { useAppSelector } from '../../redux/store'
-import AddOptionSlider from '../screens/BottomNavigationScreens/ChatScreen/AddOptionSlider'
-import { saveValueInLocalStorage } from '../helper'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import NavigationParams from "../types/navigationParams";
+import Routes from "../constants/routes";
+import BottomNavigationBar from "./BottomNavigationBar";
+import ShowMessageScreen from "../screens/ShowMessageScreen/ShowMessageScreen";
+import DpAndUserName from "../screens/DpAndUserName";
+import { useAppSelector } from "../../redux/store";
+import AddOptionSlider from "../screens/BottomNavigationScreens/ChatScreen/AddOptionSlider";
+import { saveValueInLocalStorage } from "../helper";
+import { AppState, AppStateStatus } from "react-native";
+import { doc, updateDoc } from "firebase/firestore";
+import { firestoreDB } from "../../firebaseconfig";
 
-const Stack = createNativeStackNavigator<NavigationParams>()
+const Stack = createNativeStackNavigator<NavigationParams>();
 
 const AppStack: FC = () => {
   const { loggedIn, id, mobileNumber } = useAppSelector(
-    state => state.user.value
-  )
+    (state) => state.user.value
+  );
 
   useEffect(() => {
     if (id) {
-      saveValueInLocalStorage('userInfo', JSON.stringify({ id, mobileNumber }))
+      saveValueInLocalStorage("userInfo", JSON.stringify({ id, mobileNumber }));
     } else {
       saveValueInLocalStorage(
-        'userInfo',
-        JSON.stringify({ id, mobileNumber: '' })
-      )
+        "userInfo",
+        JSON.stringify({ id, mobileNumber: "" })
+      );
     }
-  }, [id, mobileNumber])
+  }, [id, mobileNumber]);
+
+  useEffect(() => {
+    if (id) {
+      AppState.addEventListener("change", _handleAppStateChange);
+    }
+
+    return () => {
+      if (id) {
+        AppState.removeEventListener("change", _handleAppStateChange);
+      }
+    };
+  }, [id]);
+
+  const _handleAppStateChange = async (nextAppState: AppStateStatus) => {
+    const userDocFef = doc(firestoreDB, "users", id);
+
+    if (nextAppState === "active") {
+      console.log("Online");
+
+      if (id && loggedIn) {
+        await updateDoc(userDocFef, {
+          isOnline: true,
+        });
+      }
+    }
+
+    if (nextAppState === "background") {
+      console.log("Offline");
+
+      if (id && loggedIn) {
+        await updateDoc(userDocFef, {
+          isOnline: false,
+        });
+      }
+    }
+  };
 
   return (
     <Stack.Navigator>
@@ -50,7 +89,7 @@ const AppStack: FC = () => {
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
-export default AppStack
+export default AppStack;
