510424e9a1785478ed6c8faebae914196db7460c
Deepanshu Tiwari <ddepu11@gmail.com>
2022-03-24 19:45:12 +0530

    feat: change props and  use React.memo and useMemo for performance

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,34 +1,32 @@
-import { Box, Image, Text } from 'native-base'
-import { FC } from 'react'
+import { NavigationProp, useNavigation } from '@react-navigation/native'
+import { Box, Image, Text, View } from 'native-base'
+import { FC, useMemo, memo } from 'react'
 import { TouchableOpacity } from 'react-native'
 import Routes from '../constants/routes'
 import { whenWasTheDocUpdatedOrCreated } from '../helper'
+import NavigationParams from '../types/navigationParams'
 
 interface Props {
-  item: {
-    chatId?: string
-    fullName?: string
-    updatedOn: number
-    recentText?: string
-    youHaveRead?: boolean
-    displayPicUrl?: string
-    navigation: any
-    otherPersonId: string
-  }
+  chatId: string
+  fullName: string
+  updatedOn: number
+  recentText?: string
+  youHaveRead?: boolean
+  displayPicUrl: string
+  otherPersonId: string
 }
 
 const Chat: FC<Props> = ({
-  item: {
-    displayPicUrl,
-    fullName,
-    chatId,
-    updatedOn,
-    navigation,
-    recentText,
-    youHaveRead,
-    otherPersonId,
-  },
+  displayPicUrl,
+  fullName,
+  chatId,
+  updatedOn,
+  recentText,
+  youHaveRead,
+  otherPersonId,
 }) => {
+  const navigation = useNavigation<NavigationProp<NavigationParams>>()
+
   const handleChatClick = () => {
     navigation.navigate(Routes.ShowMessageScreen, {
       chatId,
@@ -40,53 +38,57 @@ const Chat: FC<Props> = ({
 
   let lastUpdatedWhen: string = ''
 
-  lastUpdatedWhen = whenWasTheDocUpdatedOrCreated(updatedOn)
+  lastUpdatedWhen = useMemo(
+    () => whenWasTheDocUpdatedOrCreated(updatedOn),
+    [updatedOn]
+  )
 
   return (
-    <TouchableOpacity onPress={handleChatClick}>
-      <Box
-        flexDirection={'row'}
-        bgColor={'#FFFFFF'}
-        paddingX={'3'}
-        paddingY={'2'}
-        mb={'3'}
-        borderRadius={'6'}
-        shadow={'0.5'}
-      >
-        <Image
-          source={{ uri: displayPicUrl }}
-          alt={fullName}
-          width={'52'}
-          height={'52'}
-          borderRadius={14}
-        />
+    <View flex={1}>
+      <TouchableOpacity onPress={handleChatClick}>
+        <Box
+          flexDirection={'row'}
+          bgColor={'#FFFFFF'}
+          paddingX={'3'}
+          paddingY={'2'}
+          mb={'3'}
+          borderRadius={'6'}
+          shadow={'0.5'}
+        >
+          <Image
+            source={{ uri: displayPicUrl }}
+            alt={fullName}
+            width={'52'}
+            height={'52'}
+            borderRadius={14}
+          />
 
-        <Box ml={'5'}>
-          <Text fontSize={14} fontWeight={500} lineHeight={'23.5'}>
-            {fullName}
-          </Text>
+          <Box ml={'5'}>
+            <Text fontSize={14} fontWeight={500} lineHeight={'23.5'}>
+              {fullName}
+            </Text>
 
-          <Text
-            fontSize={13}
-            fontWeight={400}
-            lineHeight={'18'}
-            opacity={youHaveRead ? '0.5' : '1'}
-          >
-            {recentText}
-          </Text>
-        </Box>
+            <Text
+              fontSize={13}
+              fontWeight={400}
+              lineHeight={'18'}
+              opacity={youHaveRead ? '0.5' : '1'}
+            >
+              {recentText}
+            </Text>
+          </Box>
 
-        <Box position={'absolute'} right='4' top={3} alignItems='flex-end'>
-          <Text
-            fontWeight={'400'}
-            fontSize={'12'}
-            lineHeight={'18'}
-            opacity={'0.5'}
-          >
-            {lastUpdatedWhen}
-          </Text>
+          <Box position={'absolute'} right='4' top={3} alignItems='flex-end'>
+            <Text
+              fontWeight={'400'}
+              fontSize={'12'}
+              lineHeight={'18'}
+              opacity={'0.5'}
+            >
+              {lastUpdatedWhen}
+            </Text>
 
-          {/* {!youHaveRead && (
+            {/* {!youHaveRead && (
             <Button
               mt="2"
               padding={0}
@@ -105,10 +107,21 @@ const Chat: FC<Props> = ({
               </Text>
             </Button>
           )} */}
+          </Box>
         </Box>
-      </Box>
-    </TouchableOpacity>
+      </TouchableOpacity>
+    </View>
   )
 }
 
-export default Chat
+const areEqual = (prevProps: Props, nextProps: Props) => {
+  const { chatId } = nextProps
+  const { chatId: prevChatId } = prevProps
+
+  /*if the props are equal, it won't update*/
+  const isSelectedEqual = chatId === prevChatId
+
+  return isSelectedEqual
+}
+
+export default memo(Chat, areEqual)
